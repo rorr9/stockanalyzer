8bbb08ca61c7a3ef39f3fd234940764e97c6422e
Mathew McCanty <omega@OMEGA-07X2>
2019-07-10 21:56:48 -0400

    Removed weird test flag in YahooScraper. Added NullPointerException handler (have to change log message, I copied and pasted it from the IOException above) Rewrote scrapeSingleSummaryData method Added a lot of documentation comments

--- src/stockreporter/scrappers/YahooScraper.java
+++ src/stockreporter/scrappers/YahooScraper.java
@@ -1,15 +1,15 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Stock Analyzer Project | YahooScraper.java
  */
 package stockreporter.scrappers;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import stockreporter.daomodels.StockSummary;
 import stockreporter.daomodels.StockTicker;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.jsoup.Connection;
@@ -24,24 +24,30 @@ import stockreporter.daomodels.StockDateMap;
 
 
 /**
- * Scrap Yahoo stock financial data
+ * This class scrapes pages for each tracked stock ticker symbol from
+ * Yahoo Finance  
+ * 
+ * Yahoo Finance creates a page for each ticker symbol
+ * with the url "http://finance.yahoo.com/quote/[symbol]"  Each page
+ * is populated with a large table that carries each data point in a
+ * different <td> tag
+ * 
+ * This class iterates through all <td> tags, compares their attributes
+ * to the list of required stock data, and stores their plaintext contents
+ * in the matching StockSummary parameter.
  */
 public class YahooScraper extends StockScraper {
-    
-    /**
-     * default constructor
-     */
-    //First act of making my new branch is going to be to get rid of this
-    //weird test flag
+    //Top-level object references
     private Document document;
     private StockSummary summaryData;
     
+    //Default Constructor, will be refactored after ScraperFactory is implemented
     public YahooScraper(){
         super();
     }
     
     /**
-     * Scrap summary data
+     * Scrape summary data for each StockTicker in the superclass's List<StockTicker>
      */
     public void scrapeAllSummaryData(){
         for(StockTicker stockTicker: stockTickers)
@@ -49,11 +55,11 @@ public class YahooScraper extends StockScraper {
     }
     
     /**
-     * Scrap summary data by stock ticker
+     * Scrape summary data by stock ticker
      * @param stockTicker 
      */
     public void scrapeSingleSummaryData(StockTicker stockTicker){     
-        System.out.println("Scrapping: "+stockTicker.getSymbol());
+        System.out.println("Scraping: "+stockTicker.getSymbol());
         String url = "https://finance.yahoo.com/quote/"+stockTicker.getSymbol().toLowerCase();
         try {
             Connection jsoupConn = Jsoup.connect(url);
@@ -65,93 +71,121 @@ public class YahooScraper extends StockScraper {
             stockDateMap.setDate(new SimpleDateFormat("MM-dd-yyyy").format(new Date()));
             int last_inserted_id = dao.insertStockDateMap(stockDateMap);
         
-            Element table1 = document.select("table").get(0);
-            Elements rows = table1.select("tr");    
             summaryData = new StockSummary();
             
             summaryData.setStockDtMapId(last_inserted_id);
             
-            int rowNum=0;
-            String prevClosePrice = rows.get(rowNum).select("td").get(1).text();
-            summaryData.setPrevClosePrice(Utility.convertStringCurrency(Utility.isBlank(prevClosePrice)?"0":prevClosePrice));
-            rowNum++;
-            
-            String openPrice = rows.get(rowNum).select("td").get(1).text();
-            summaryData.setOpenPrice(Utility.convertStringCurrency(Utility.isBlank(openPrice)?"0":openPrice));
-            rowNum++;
-            
-            String bidPrice = rows.get(rowNum).select("td").get(1).text();
-            summaryData.setBidPrice(Utility.convertStringCurrency(Utility.isBlank(bidPrice)?"0":bidPrice));
-            rowNum++;
-            
-            String askPrice = rows.get(rowNum).select("td").get(1).text();
-            summaryData.setAskPrice(Utility.convertStringCurrency(Utility.isBlank(askPrice)?"0":askPrice));
-            rowNum++;
-                       
-            String daysRangeMin = Utility.getRangeMinAndMax(rows.get(rowNum).select("td").get(1).text())[0].trim();
-            summaryData.setDaysRangeMin(Utility.convertStringCurrency(Utility.isBlank(daysRangeMin)?"0":daysRangeMin));
-            String daysRangeMax = Utility.getRangeMinAndMax(rows.get(rowNum).select("td").get(1).text())[1].trim();
-            summaryData.setDaysRangeMax(Utility.convertStringCurrency(Utility.isBlank(daysRangeMax)?"0":daysRangeMax)); 
-            rowNum++;
-                    
-            String fiftyTwoWeeksMin = Utility.getRangeMinAndMax(rows.get(rowNum).select("td").get(1).text())[0].trim();
-            summaryData.setFiftyTwoWeeksMin(Utility.convertStringCurrency(Utility.isBlank(fiftyTwoWeeksMin)?"0":fiftyTwoWeeksMin));
-            String fiftyTwoWeeksMax = Utility.getRangeMinAndMax(rows.get(rowNum).select("td").get(1).text().trim())[1].trim();
-            summaryData.setFiftyTwoWeeksMax(Utility.convertStringCurrency(Utility.isBlank(fiftyTwoWeeksMax)?"0":fiftyTwoWeeksMax));
-            rowNum++;
-            
-            String volume = rows.get(rowNum).select("td").get(1).text();
-            summaryData.setVolume(Utility.convertStringCurrency(Utility.isBlank(volume)?"0":volume).longValue());
-            rowNum++;
-            
-            String avgVolume = rows.get(rowNum).select("td").get(1).text();
-            summaryData.setAvgVolume(Utility.convertStringCurrency(Utility.isBlank(avgVolume)?"0":avgVolume).longValue());
+            //Scrape all <td> tags from the document
+            Elements yahooTDs = document.select("td");
 
-            
-            rowNum=0;
-            Element table2 = document.select("table").get(1);
-            rows = table2.select("tr");    
-            
-            String marketCap = rows.get(rowNum).select("td").get(1).text();
-            summaryData.setMarketCap(Utility.convertStringCurrency(Utility.isBlank(marketCap)?"0":marketCap));
-            rowNum++;
-            
-            String betaCoefficient = rows.get(rowNum).select("td").get(1).text();
-            summaryData.setBetaCoefficient(Utility.convertStringCurrency(Utility.isBlank(betaCoefficient)?"0":betaCoefficient));
-            rowNum++;
-            
-            String peRatio = rows.get(rowNum).select("td").get(1).text();
-            summaryData.setPeRatio(Utility.convertStringCurrency(Utility.isBlank(peRatio)?"0":peRatio));
-            rowNum++;
-            
-            String eps = rows.get(rowNum).select("td").get(1).text();
-            summaryData.setEps(Utility.convertStringCurrency(Utility.isBlank(eps)?"0":eps));
-            rowNum++;
-            
-            String earningDate = rows.get(rowNum).select("td").get(1).text();
-            summaryData.setEarningDate(earningDate);
-            rowNum++;
-            
-            String dividend = Utility.getNumberBeforeParantheses(rows.get(rowNum).select("td").get(1).text()).trim();
-            summaryData.setDividentYield(Utility.convertStringCurrency(Utility.isBlank(dividend)?"0":dividend));
-            rowNum++;
-            
-            String exDividendDate = rows.get(rowNum).select("td").get(1).text();
-            summaryData.setExDividentDate(exDividendDate);
-            rowNum++;
-            
-            String onYearTargetEst = rows.get(rowNum).select("td").get(1).text();
-            summaryData.setOneYearTargetEst(Utility.convertStringCurrency(Utility.isBlank(onYearTargetEst)?"0":onYearTargetEst));
-            
+            //Scan each <td> for attribute "data-test" and record its text if it matches various
+            //price data fields
+            for(Element td: yahooTDs){
+                //Check that each td has an attribute named "data-test"
+                if (td.hasAttr("data-test")){
+                    //Create a map of the td's data-set attributes <AttributeTag, Value>
+                    Map<String, String> tdData = td.dataset();
+                    //Get the plain-text from each td
+                    String tdText = td.text();
+                    
+                    //Declare three BigDecimals for a single amount, range minimum, and range maximum
+                    BigDecimal tdAmount = Utility.convertStringCurrency("0");
+                    BigDecimal tdMin = Utility.convertStringCurrency("0");
+                    BigDecimal tdMax = Utility.convertStringCurrency("0");
+                    
+                    //Ensure tdText doesn't contain a date
+                    if (!tdText.contains(",")) {
+                        //Check if tdText is a Range (contains a hyphen)
+                        if(tdText.contains("-")){
+                            //Use Utility.getRangeMinAndMax to break tdText into two Strings
+                            String rangeMin = Utility.getRangeMinAndMax(tdText)[0].trim();
+                            String rangeMax = Utility.getRangeMinAndMax(tdText)[1].trim();
+                            //Convert those Strings to currency format
+                            tdMin = Utility.convertStringCurrency(Utility.isBlank(rangeMin)?"0":rangeMin);
+                            tdMax = Utility.convertStringCurrency(Utility.isBlank(rangeMax)?"0":rangeMax);
+                        //Else, check if tdText has a parenthesis, and get only the text before it
+                        } else if (tdText.contains("(")){
+                            String dividend = Utility.getNumberBeforeParantheses(tdText).trim();
+                            //Convert that text to currency format
+                            tdAmount = Utility.convertStringCurrency(Utility.isBlank(dividend)?"0":dividend);
+                        //Else it's a normal number and can be converted to currency format
+                        }else {
+                            tdAmount = Utility.convertStringCurrency(Utility.isBlank(tdText)?"0":tdText);
+                        }
+                    }
+                    //Case statement for each data field (dataset() stores each attribute minus its HTML5 "data-" prefix
+                    //so this switch just uses Map.get("test"))
+                    switch(tdData.get("test")){
+                        //Cases should hold up regardless of Yahoo table order
+                        case "PREV_CLOSE-value" :
+                            summaryData.setPrevClosePrice(tdAmount);
+                            break;
+                        case "OPEN-value" :
+                            summaryData.setOpenPrice(tdAmount);
+                            break;
+                        case "BID-value" :
+                            summaryData.setBidPrice(tdAmount);
+                            break;
+                        case "ASK-value" :
+                            summaryData.setAskPrice(tdAmount);
+                            break;
+                        case "DAYS_RANGE-value" :
+                            summaryData.setDaysRangeMin(tdMin);
+                            summaryData.setDaysRangeMax(tdMax);
+                            break;
+                        case "FIFTY_TWO_WK_RANGE-value" :
+                            summaryData.setFiftyTwoWeeksMin(tdMin);
+                            summaryData.setFiftyTwoWeeksMax(tdMax);
+                            break;
+                        case "TD_VOLUME-value" :
+                            //This uses long instead of BigDecimal
+                            summaryData.setVolume(tdAmount.longValue());
+                            break;
+                        case "AVERAGE_VOLUME_3MONTH-value" :
+                            //This uses long instead of BigDecimal
+                            summaryData.setAvgVolume(tdAmount.longValue());
+                            break;
+                        case "MARKET_CAP-value" :
+                            summaryData.setMarketCap(tdAmount);
+                            break;
+                        case "BETA_3Y-value" :
+                            summaryData.setBetaCoefficient(tdAmount);
+                            break;
+                        case "PE_RATIO-value" :
+                            summaryData.setPeRatio(tdAmount);
+                            break;
+                        case "EPS_RATIO-value" :
+                            summaryData.setEps(tdAmount);
+                            break;
+                        case "EARNINGS_DATE-value" :
+                            //This uses the plaintext tdText, not a converted value
+                            summaryData.setEarningDate(tdText);
+                            break;
+                        case "DIVIDEND_AND_YIELD-value" :
+                            summaryData.setDividentYield(tdAmount);
+                            break;
+                        case "EX_DIVIDEND_DATE-value" :
+                            //This uses the plaintext tdText, not a converted value
+                            summaryData.setExDividentDate(tdText);
+                            break;
+                        case "ONE_YEAR_TARGET_PRICE-value" :
+                            summaryData.setOneYearTargetEst(tdAmount);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
             dao.insertStockSummaryData(summaryData);
             
         } catch (IOException ex) {
+            //Generic log message for failed Jsoup connection
             Logger.getLogger(StockReporter.class.getName()).log(Level.SEVERE, null, ex);
         }
         catch (NullPointerException ex){
-            //Yahoo can and will make a blank page if given the URL of an invalid stock ticker symbol.
-            //This exception will be thrown by the scraper if it tries to scrape a blank page.
-            Logger.getLogger(StockReporter.class.getName()).log(Level.SEVERE, null, ex);
+            //Yahoo can and will make a blank page if given the URL of an invalid stock ticker symbol
+            //This exception will be thrown by the scraper if it tries to scrape a blank page
+            Logger.getLogger(StockReporter.class.getName()).log(Level.SEVERE, "Empty Page Found for given symbol: " + stockTicker.getSymbol(), ex);
         }
     }
 }

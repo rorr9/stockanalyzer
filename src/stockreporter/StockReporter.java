bb01677fbca57b15b82e427e8dd585f1d181feb0
craigscheiderer <craigscheiderer@craigs-imac.fios-router.home>
2019-07-19 15:12:36 -0400

    StockReporter and Constants

--- src/stockreporter/StockReporter.java
+++ src/stockreporter/StockReporter.java
@@ -2,8 +2,9 @@ package stockreporter;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import stockreporter.scrappers.YahooScraper;
-import stockreporter.scrappers.InvestopediaScraper;
+import stockreporter.scrapers.YahooScraper;
+import stockreporter.scrapers.InvestopediaScraper;
+import stockreporter.scrapers.FidelityScraper;
 
 /**
  * Main class for scrapping the data
@@ -20,12 +21,16 @@ public class StockReporter {
         logger.log(Level.INFO, "Create scraper instances");
         InvestopediaScraper investopediaScraper = new InvestopediaScraper();
         YahooScraper yahooScraper = new YahooScraper();
+        FidelityScraper fidelityScraper = new FidelityScraper();
         
         logger.log(Level.INFO, "Scrap summary data for Yahoo...");
         yahooScraper.scrapeAllSummaryData();
         
         logger.log(Level.INFO, "Scrap summary data for Investopedia...");
         investopediaScraper.scrapeAllSummaryData();
+        
+        logger.log(Level.INFO, "Scrap summary data for Fidelity...");
+        fidelityScraper.scrapeAllSummaryData();
         
     }
 }
